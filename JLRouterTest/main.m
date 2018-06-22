e1b458b949e08d592d47d89b1759daeb56cc694f
Fu Li <2636015706@qq.com>
2018-06-22 11:18:51 +0800

    Initial Commit

---
+++ JLRouterTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JLRouterTest
+//
+//  Created by Lfur on 2018/6/22.
+//  Copyright © 2018年 Lfur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
